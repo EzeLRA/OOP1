e82f00fcb47c36512991d486f33737eb634c9ed1
EzeLRA <ezeleocraft@gmail.com>
2025-09-01 16:31:52 -0300

    ejercicio3

---
+++ ejercicio3/src/main/java/ar/edu/unlp/info/oo1/ejercicio3/Item.java
@@ -0,0 +1,16 @@
+package ar.edu.unlp.info.oo1.ejercicio3;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Item {
+    private String detalle;
+    private int cantidad;
+    private float costoUnitario;
+    
+    public float costo(){
+        
+    }
+    
+}
---
+++ ejercicio3/src/main/java/ar/edu/unlp/info/oo1/ejercicio3/Presupuesto.java
@@ -0,0 +1,21 @@
+package ar.edu.unlp.info.oo1.ejercicio3;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class Presupuesto {
+    private LocalDate fecha;
+    private String cliente;
+    
+    public void agregarItem(Item item){
+        
+    }
+    
+    public float calcularTotal(){
+        
+    }
+    
+}
